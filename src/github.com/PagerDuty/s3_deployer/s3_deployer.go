d91a996d702cf5ead9ee90fa39eb3cf1b9d6d59c
Rob Ottaway <rottaway@pagerduty.com>
2015-04-14 15:39:39 -0700

    Working version of deployment from S3

--- src/github.com/PagerDuty/s3_deployer/s3_deployer.go
+++ src/github.com/PagerDuty/s3_deployer/s3_deployer.go
@@ -5,7 +5,6 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/aws/awsutil"
 	"github.com/awslabs/aws-sdk-go/service/s3"
 	"github.com/docopt/docopt-go"
 	"github.com/smallfish/simpleyaml"
@@ -14,32 +13,51 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
+const groupWriteMask os.FileMode = os.FileMode(0020)
+const userExecuteMask os.FileMode = os.FileMode(0100)
+const otherNoneMask os.FileMode = os.FileMode(0770)
+
 // StorageConfiguration is the object containing the details used in deployment.
 // It is passed to methods that are in need of instruction.
-type StorageConfiguration struct {
+type InstallConfiguration struct {
 	applicationName    string // name of application being deployed
-	deploymentLocation string // were we end up putting versions of app
+	commitHash         string // the Git sha of the version being released
+	deploymentLocation string // were we end up putting versions of apps we deploy
+	releaseLocation    string // were given release will go (subfolder of deploymentLocation)
+	scripts            string // a comma separated list of scripts, eg bin/one.sh,cmd/two.rb
+	groupWritable      bool   // if true g+w files in deployment
+	removeOther        bool   // if true o-rwx for files
 }
 
 func main() {
-	usage := `
+	usage := `S3 Deployer - installs archived applications from S3
 
     Usage:
-        s3_deployer install <version>
+        s3_deployer listbucket [--bucket=<bucketname>] [--matching=<matcher>]
+        s3_deployer install <application> <version> [--scripts=<scriptLocations>]
+		    [--groupwritable] [--removeother]
+        s3_deployer -h | --help
 
     Options:
-        -h --help    Show this screen`
+        -h --help                    Show this screen.
+        --bucket=<bucketname>        Name of bucket to target [default: pd-release].
+		--scripts=<scriptLocations>  Comma separated list of scripts relative to root of distribution.
+        --matching=<matcher>         A regex to use in matching keys in bucket
+		--groupwritable              If flag set will set all deployed files to g+w
+		--removeother                Remove any permissions for other`
 
 	arguments, _ := docopt.Parse(usage, nil, true, "S3 Deployer 0.1.0", false)
-	fmt.Println(arguments)
 
 	content, err := ioutil.ReadFile("/etc/pagerduty/devtools.yml")
 	if err != nil {
 		panic(err)
 	}
+
+	// read the devtools configuration file
 	yaml, err := simpleyaml.NewYaml(content)
 	if err != nil {
 		panic(err)
@@ -47,75 +65,125 @@ func main() {
 	access_key_id, err := yaml.Get("access_key_id").String()
 	secret_access_key, err := yaml.Get("secret_access_key").String()
 
+	// build a S3 client
 	creds := aws.Creds(access_key_id, secret_access_key, "")
 	s3_config := &aws.Config{Region: "us-west-1", Credentials: creds}
 	s3_svc := s3.New(s3_config)
 
-	//list_buckets(s3_svc)
-	key := "legoland/legoland-09924950.zip" // should be passed as arg
-	err = InstallArtifact(s3_svc, key, "/var/pd-hg-assets/assets", "junk.zip")
-	if awserr := aws.Error(err); awserr != nil {
-		if awserr.Code == "NoSuchKey" {
-			fmt.Printf("The key '%s' does not exist in S3 repository!\n", key)
-		} else {
-			fmt.Println("Error:", awserr.Code, awserr.Message)
+	if arguments["listbucket"] == true {
+		bucket, _ := arguments["--bucket"].(string)
+		matcher, _ := arguments["--matching"].(string)
+		ListBucket(s3_svc, bucket, matcher)
+	} else if arguments["install"] == true {
+		conf := BuildInstallConfig(arguments)
+		err = InstallArtifact(s3_svc, conf)
+		if awserr := aws.Error(err); awserr != nil {
+			if awserr.Code == "NoSuchKey" {
+				fmt.Printf(
+					"Unable to find artifact for application '%s' commit hash '%s' in S3 repository!\n",
+					conf.applicationName, conf.commitHash)
+			} else {
+				fmt.Println("Error:", awserr.Code, awserr.Message)
+			}
+		} else if err != nil {
+			panic(err)
 		}
-	} else if err != nil {
-		panic(err)
 	}
+}
+
+// BuildInstallConfig takes the arguments and returns a config object
+func BuildInstallConfig(arguments map[string]interface{}) *InstallConfiguration {
+	applicationName, _ := arguments["<application>"].(string)
+	commitHash, _ := arguments["<version>"].(string)
+	scripts, _ := arguments["--scripts"].(string)
+	groupWritable, _ := arguments["--groupwritable"].(bool)
+	removeOther, _ := arguments["--removeother"].(bool)
+	deploymentLocation := "/usr/local/legoland"
+	conf := &InstallConfiguration{
+		applicationName:    applicationName,
+		commitHash:         commitHash,
+		deploymentLocation: deploymentLocation,
+		releaseLocation:    fmt.Sprintf("%s/releases/%s", deploymentLocation, commitHash),
+		scripts:            scripts,
+		groupWritable:      groupWritable,
+		removeOther:        removeOther,
+	}
+	return conf
 }
 
 // ListBucket returns listing of contents of given bucket.
 // Will print the raw JSON structure returned by S3 to stdout.
-func ListBucket(s3_svc *s3.S3, bucketName string) {
+func ListBucket(s3_svc *s3.S3, bucketName, matcher string) {
 	params := &s3.ListObjectsInput{
 		Bucket: aws.String(bucketName),
 	}
 	resp, err := s3_svc.ListObjects(params)
 	if awserr := aws.Error(err); awserr != nil {
-		fmt.Println("Error:", awserr.Code, awserr.Message)
+		if awserr.Code == "AccessDenied" {
+			fmt.Printf("You do not have access to the bucket '%s'\n", bucketName)
+		} else if awserr.Code == "NoSuchBucket" {
+			fmt.Printf("Bucket '%s' not found\n", bucketName)
+		} else {
+			fmt.Println("S3 returned an error:", awserr.Code, awserr.Message)
+		}
+		return
 	} else if err != nil {
 		panic(err)
 	}
-	fmt.Println(awsutil.StringValue(resp))
+	var r *regexp.Regexp = nil
+	if matcher != "" {
+		r, err = regexp.Compile(matcher)
+		if err != nil {
+			fmt.Printf("Matcher '%s' cannot be understood, should be RE2 type regex\n", matcher)
+			return
+		}
+	}
+	for _, value := range resp.Contents {
+		if r != nil {
+			if r.MatchString(*value.Key) {
+				fmt.Println(*value.Key)
+			}
+		} else {
+			fmt.Println(*value.Key)
+		}
+	}
 }
 
 // InstallArtifact will download and install given artifact.
 // Will return error if unable to download or install.
 // Be aware error may be aws.Error
-func InstallArtifact(s3_svc *s3.S3, s3Key, installLocation, filename string) error {
+func InstallArtifact(s3_svc *s3.S3, conf *InstallConfiguration) error {
 	// check if file already exists on machine, if so do nothing
+	fi, _ := os.Stat(conf.releaseLocation)
+	if fi != nil {
+		fmt.Printf(
+			"Location '%s' already exists! Release '%s' already installed?\n",
+			conf.releaseLocation, conf.commitHash)
+		return nil
+	}
 
 	// download the artifact, unzip it to install location
-	artifact, err := DownloadArtifact(s3_svc, s3Key)
+	artifactLocation, err := DownloadArtifact(s3_svc, conf.applicationName, conf.commitHash)
 	if err != nil {
-		return err
-	}
-
-	artifactLocation := fmt.Sprintf("%s/%s", installLocation, filename)
-
-	// write out the S3 artifact, ensure stream is closed on method exit
-	err = WriteOutArtifact(artifact.Body, artifactLocation)
-	if err != nil {
-		return err
-	}
-
-	if err = artifact.Body.Close(); err != nil {
 		return err
 	}
 
 	// explode the contents of artifact
-	err = UnzipArtifact(artifactLocation, "/usr/local/legoland/releases/junk")
+	err = UnzipArtifact(artifactLocation, conf.releaseLocation)
 	if err != nil {
 		panic(err)
 	}
 
+	FinalizePermissions(conf)
+
 	return nil
 }
 
-// GetArtifact downloads the requested zip archive file from S3.
+// DownloadArtifact downloads the matching zip archive file from S3.
 // Returns the s3.GetObjectOutput object representing this file, or error.
-func DownloadArtifact(s3_svc *s3.S3, key string) (*s3.GetObjectOutput, error) {
+func DownloadArtifact(s3_svc *s3.S3, applicationName, commitHash string) (string, error) {
+	key := fmt.Sprintf("%s/%s-%s.zip", applicationName, applicationName, commitHash)
+
 	// query S3 for the artifact being asked for
 	get_obj_params := &s3.GetObjectInput{
 		Bucket: aws.String("pd-release"),
@@ -123,44 +191,46 @@ func DownloadArtifact(s3_svc *s3.S3, key string) (*s3.GetObjectOutput, error) {
 	}
 	resp, err := s3_svc.GetObject(get_obj_params)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return resp, nil
-}
 
-// WriteOutArtifact will take the given Reader interface and stream it's contents to the given file.
-// Will return error if unable to write file or issues flushing/closing writer.
-func WriteOutArtifact(reader io.ReadCloser, artifactLocation string) error {
-	// create or overwrite file
-	fo, err := os.Create(artifactLocation)
+	prefix := fmt.Sprintf("s3_deployer-%s-%s", applicationName, commitHash)
+	fo, err := ioutil.TempFile(os.TempDir(), prefix)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	w := bufio.NewWriter(fo)
 	buf := make([]byte, 1024)
 	for {
-		n, err := reader.Read(buf)
+		n, err := resp.Body.Read(buf)
 		if err != nil && err != io.EOF {
-			return err
+			return "", err
 		}
 		if n == 0 {
 			break
 		}
 		if _, err := w.Write(buf[:n]); err != nil {
-			return err
+			return "", err
 		}
 	}
+
+	if err = resp.Body.Close(); err != nil {
+		return "", err
+	}
+
 	if err := w.Flush(); err != nil {
-		return err
+		return "", err
 	}
+
 	if err := fo.Close(); err != nil {
-		return err
+		return "", err
 	}
-	return nil
+
+	return fo.Name(), nil
 }
 
-// Unzip opens given src zip file and dumps it's contents in dest.
+// UnzipArtifact opens given src zip file and dumps it's contents in dest.
 func UnzipArtifact(src, dest string) error {
 	r, err := zip.OpenReader(src)
 	if err != nil {
@@ -205,12 +275,45 @@ func UnzipArtifact(src, dest string) error {
 	return nil
 }
 
-// FinalizePermissions  updates permissions on the files unzipped from artifact
-func FinalizePermissions() {
-	// run("chmod -R g+w #{lr}") if fetch(:group_writable, true
-	// run("chmod u+x #{init_script(lr)}"):
-	//     path + "/scripts/" + fetch(:application) + ".sh"
-	// run("chmod u+x #{ping_script(lr)}"):
-	//     path + "/scripts/ping.sh"
-	// for each script run("chmod u+x #{get_script_path(script, lr)}")
+// FinalizePermissions updates permissions on the files unzipped from artifact
+func FinalizePermissions(conf *InstallConfiguration) {
+	fmt.Printf("Updating any permissions as needed for files under '%s'\n", conf.releaseLocation)
+
+	filepath.Walk(conf.releaseLocation, func(path string, info os.FileInfo, err error) error {
+		perms := info.Mode()
+
+		// make files group writable if requested
+		if conf.groupWritable {
+			perms |= groupWriteMask
+		}
+		// remove all other perms if requested
+		if conf.removeOther {
+			perms = perms & otherNoneMask
+		}
+		// make any scripts executable
+		if strings.HasSuffix(path, ".sh") {
+			perms |= userExecuteMask
+		}
+		// apply permission changes
+		fm := os.FileMode(perms)
+		os.Chmod(path, fm)
+
+		return nil
+	})
+
+	if conf.scripts != "" {
+		fmt.Println("Adding u+x for given scripts:")
+		parts := strings.Split(conf.scripts, ",")
+		for idx, val := range parts {
+			parts[idx] = strings.Trim(val, " ")
+			fullPath := fmt.Sprintf("%s/%s", conf.releaseLocation, parts[idx])
+			fi, _ := os.Stat(fullPath)
+			if fi != nil {
+				var mode os.FileMode = fi.Mode() | os.FileMode(userExecuteMask)
+				os.Chmod(fullPath, mode)
+			} else {
+				fmt.Printf("Unable to update permissions for script '%s', does it exist?\n", fullPath)
+			}
+		}
+	}
 }
